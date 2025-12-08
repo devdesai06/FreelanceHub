bda156b97df1391c84c3a5217d058a254d941c5a
devdesai06 <devdesai8790@gmail.com>
2025-12-08 21:40:10 +0530

    Fix: installed bcryptjs

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import projectRoutes from './routes/project.route.js'
 import bidRoutes from './routes/bid.route.js'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-app.use(cors({origin: 'http://localhost:3000',credentials:true}));
+app.use(cors({origin: process.env.FRONTEND_URL,credentials:true}));
 app.use(cookieParser());
 //routing middleware
 app.use(express.json());   
